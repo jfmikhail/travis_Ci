e4559705d57840f7c4c18b9d3debf260d4ae8263
Joseph <joseph.mikhail.developer@gmail.com>
2022-09-18 10:35:11 +0200

    travis env variables

--- server.js
+++ server.js
@@ -6,6 +6,9 @@ async function main() {
   while(true) {
     //log 
     console.log('Containers rule!');
+
+    const favoriteFood = process.env.FAVORITE_FOOD;
+    console.log(`My favorite food is ${favoriteFood}`);
     await sleep(5000);
   }
 }
